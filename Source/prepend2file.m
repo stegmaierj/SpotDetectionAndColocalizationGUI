40f6cc3c4aeb8117f449622ece407784ff0ec3b4
Stegmaier <johannes.stegmaier@kit.edu>
2017-07-21 08:24:46 +0200

    added modified copyright statement

--- Source/prepend2file.m
+++ Source/prepend2file.m
@@ -1,4 +1,4 @@
- function prepend2file( string, filename, newline )
+function prepend2file( string, filename, newline )
 % newline: is an optional boolean, that if true will append a \n to the end
 % of the string that is sent in such that the original text starts on the
 % next line rather than the end of the line that the string is on
